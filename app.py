d027d214517dac96e38bfb1575c500fc278843b5
Sander Nemvalts <snemvalts@gmail.com>
2018-10-28 23:56:46 +0200

    basic socketio demo

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO, send, emit
+
+
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-
+app.config['SECRET_KEY'] = 'secret!'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def hello():
     return render_template('hello.html')
+
+@app.route("/chat/")
+def chat():
+    return render_template('chat.html')
+
+@socketio.on('message_sent')
+def handle_message(message):
+    print('MESSAGE RECEIVED')
+    emit('message_received', {'data':'yooo'})
+
+if __name__ == '__main__':
+    socketio.run(app)
