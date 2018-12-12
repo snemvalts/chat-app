26e37e6815bf063a48204f1e07fa8818b34269f4
Sander Nemvalts <snemvalts@gmail.com>
2018-12-12 18:02:08 +0200

    local network

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def user_connected(recipient):
     emit('contacted', user, room=recipient_sid)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=5000)
